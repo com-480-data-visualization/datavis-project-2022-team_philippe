21e787a8b7cfcf6005e22857bdc5d8dc1c4daa54
guillaumeP <guillaume.parchet@epfl.ch>
2022-06-01 21:02:58 +0200

    Further draft bipartite + remove line breaks from descriptions

--- website/js/biparite.js
+++ website/js/biparite.js
@@ -1,5 +1,4 @@
 
-const data_file = "../data/biparite_data.csv"
 const stats = ["rating", "page", "cover", "price"]
 
 var biparite_svg = d3.select("biparite-vizu")
@@ -16,16 +15,36 @@ var stat_selection = d3.select("biparite-vizu")
                         .attr("value", function(stat){return stat})
                         .text(function(stat){return "genres VS " + String(stat);})
 
-function biparite_update(){
+function biparite_update(g, file_name, stat){
 
-  d3.csv(data_file).then(raw_data => {
+  d3.csv(file_name).then(raw_data => {
 
     var display_data = []
 
     raw_data.forEach(function(row) {
+      var buff_arr = new Array(0);
 
+      buff_arr.push(row.genre)
+      buff_arr.push(row[stat])
+      buff_arr.push(parseFloat(row.count))
+      display_data.push(buff_arr)
+
+      // TODO: colors
 
     });
+
+    console.log(display_data);
+
+    var biparite_d3 = viz.bP()
+                        .data(display_data)
+                        .min(12)
+                        .pad(1)
+                        .height(700)
+                        .width(400)
+                        .barSize(35)
+                        //.fill(e => ) TODO
+
+
 
 
 
@@ -43,7 +62,11 @@ function build_biparite_ratings(){
       .text("Genres VS Ratings")
       .style("fill", 'black');
 
-  var group = biparite_svg.append("g").attr("transform","translate(150,100)");
+  var g = biparite_svg.append("g").attr("transform","translate(150,100)");
 
-  biparite_update(g, "ratings", )
+  biparite_update(g, "../data/biparite_ratings.csv", "rating_cat")
 }
+
+window.addEventListener('load', function() {
+    build_biparite_ratings()
+})
